48490357270393153057dc182511a40ecc3d2169
Scott Talbot <s@chikachow.org>
2014-05-19 18:40:31 +1000

    Import skeleton

---
+++ STXMLDocument/STXMLDocument.h
@@ -0,0 +1,8 @@
+//  Copyright (c) 2014 Scott Talbot. All rights reserved.
+
+#import <Foundation/Foundation.h>
+
+
+@interface STXMLDocument : NSObject
+
+@end
---
+++ STXMLDocument/STXMLDocument.m
@@ -0,0 +1,8 @@
+//  Copyright (c) 2014 Scott Talbot. All rights reserved.
+
+#import "STXMLDocument.h"
+
+
+@implementation STXMLDocument
+
+@end
